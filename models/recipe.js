53d296fcdc7c21985518dc2eb5356ba4b8285863
jwen1116 <41092244+jwen1116@users.noreply.github.com>
2019-11-25 21:43:11 +0800

    Update recipe.js

--- models/recipe.js
+++ models/recipe.js
@@ -3,13 +3,9 @@ const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
     title: {type: String, required: true},
-    //ingredients: {type:String},
     image: {type: String},
     link: {type: String},
     source: {type:String},
-    //expectedCalories: {type: String},
-    //actualCalories: {type: String},
-    //nutrientInfo: {type: String},
     saved: {type: Boolean}
 });
 
